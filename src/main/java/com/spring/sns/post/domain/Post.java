a0009bf083a266feb02781a54faeea26bf48a542
mong9ssi <mong9ssi@naver.com>
2025-05-29 17:35:47 +0900

    feat: add Post entity

--- src/main/java/com/spring/sns/post/domain/Post.java
+++ src/main/java/com/spring/sns/post/domain/Post.java
@@ -1,4 +1,59 @@
 package com.spring.sns.post.domain;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Entity
 public class Post {
+
+    // 속성
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    /**
+     * 엔티티가 처음 저장되기 직전에 호출
+     * createdAt , updatedAt 필드를 현재 UTC 시간으로 초기화
+     */
+    @PrePersist
+    public void onCreate() {
+        LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    /**
+     * 엔티티가 수정되기 직전에 호출
+     * updatedAt 필드를 현재 UTC 시간으로 초기화
+     */
+    @PreUpdate
+    public void onUpdate() {
+        LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
+        this.updatedAt = now;
+    }
+
+    // 생성자
+    // JPA가 사용
+    protected Post () {}
+
+
 }
