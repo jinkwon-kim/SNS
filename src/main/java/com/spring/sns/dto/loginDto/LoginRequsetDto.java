59755098896e4b9020d7a410aca6b710c71bf3cd
김진권(Spring_7기) <sjh99021800@gmail.com>
2025-05-30 18:59:14 +0900

    add LoginRequsetDto

--- src/main/java/com/spring/sns/dto/loginDto/LoginRequsetDto.java
+++ src/main/java/com/spring/sns/dto/loginDto/LoginRequsetDto.java
@@ -1,4 +1,20 @@
 package com.spring.sns.dto.loginDto;
 
 public class LoginRequsetDto {
+    //속성
+    private String userEmail;
+    private String password;
+
+    //생성자
+
+    //기능
+
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
