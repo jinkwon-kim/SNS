3ba6cf61ba6e7f118118da062b3d772a428fbc5c
김진권(Spring_7기) <sjh99021800@gmail.com>
2025-05-30 18:10:01 +0900

    add Comments

--- src/main/java/com/spring/sns/domain/Comments.java
+++ src/main/java/com/spring/sns/domain/Comments.java
@@ -1,10 +1,51 @@
 package com.spring.sns.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Comments {
-    @Id
+
+    // 속성
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String commentText;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private String userEmail;
+
+    @Column(nullable = false)
+    private String password;
+
+    // 생성자
+    /**
+     * 기본생성자(JPA에서 사용)
+     */
+    public Comment() {}
+
+    /**
+     * createCommentRequestDto에서 사용하는 생성자
+     * @param userName
+     * @param commentText
+     */
+    public Comments(String userName, String commentText) {
+        this.userName = userName;
+        this.commentText = commentText;
+    }
+
+    // 기능
+
 }
