06b64abbcec7484cc11d55b29ab5030e8ff2c036
mong9ssi <mong9ssi@naver.com>
2025-05-30 18:07:56 +0900

    "feat: create Posts entity"

--- src/main/java/com/spring/sns/domain/Follows.java
+++ src/main/java/com/spring/sns/domain/Follows.java
@@ -3,8 +3,14 @@ package com.spring.sns.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
+import java.time.LocalDateTime;
+
 @Entity
 public class Follows {
     @Id
     private Long id;
+
+    private Long userId;
+    private Long targetId;
+    private LocalDateTime createdAt;
 }
