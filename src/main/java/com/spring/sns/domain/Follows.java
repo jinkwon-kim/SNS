75cf3e474f0db5d038f588359b77566ce384e2d2
mong9ssi <mong9ssi@naver.com>
2025-05-30 18:11:08 +0900

    "feat: create Follows entity"

--- src/main/java/com/spring/sns/domain/Follows.java
+++ src/main/java/com/spring/sns/domain/Follows.java
@@ -1,16 +1,28 @@
 package com.spring.sns.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Entity
 public class Follows {
-    @Id
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(name = "USER_ID", nullable = false)
     private Long userId;
+    @Column(name = "TARGET_ID", nullable = false)
     private Long targetId;
+    @Column(nullable = false)
     private LocalDateTime createdAt;
+
+    /**
+     * 엔티티가 처음 저장되기 직전에 호출
+     * createdAt 필드를 현재 UTC 시간으로 초기화
+     */
+    @PrePersist
+    public void onCreate() {
+        LocalDateTime now = LocalDateTime.now(ZoneOffset.UTC);
+        this.createdAt = now;
+    }
 }
